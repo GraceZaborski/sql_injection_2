5fa0720b5e40b07998fa8c1fe1ce083a6ba7ba64
Grace Zaborski <grace.zaborski@beamery.com>
2021-11-28 12:45:42 +0000

    sql-injection safe

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import { Client } from "pg";
 //and default username and password,
 //we only need to specify the (non-default) database name.
 
-const client = new Client({ database: 'guestbook' });
+const client = new Client({ database: 'testDB' });
 
 //TODO: this request for a connection will not necessarily complete before the first HTTP request is made!
 client.connect();
@@ -24,117 +24,24 @@ app.use(cors());
  */
 app.use(express.json());
 
-//When this route is called, return the most recent 100 signatures in the db
-app.get("/signatures", async (req, res) => {
-  const signatures = null; //FIXME-TASK: get signatures from db!
-  res.status(200).json({
-    status: "success",
-    data: {
-      signatures
-    },
-  });
-});
-
-app.get("/signatures/:id", async (req, res) => {
+app.get("/injection/:id", async (req, res) => {
   // :id indicates a "route parameter", available as req.params.id
   //  see documentation: https://expressjs.com/en/guide/routing.html
-  const id = parseInt(req.params.id); // params are always string type
+  const id = req.params.id; // params are always string type
 
-  const signature = null;   //FIXME-TASK get the signature row from the db (match on id)
+  const result = await client.query(`select * from testdb where id = $1`, [`${id}`]);   //FIXME-TASK get the signature row from the db (match on id)
+  const isResult = result.rowCount === 1;
 
-  if (signature) {
+  if (isResult) {
     res.status(200).json({
       status: "success",
-      data: {
-        signature,
-      },
+      data: result.rows
     });
   } else {
     res.status(404).json({
       status: "fail",
       data: {
-        id: "Could not find a signature with that id identifier",
-      },
-    });
-  }
-});
-
-app.post("/signatures", async (req, res) => {
-  const { name, message } = req.body;
-  if (typeof name === "string") {
-    const createdSignature = null; //FIXME-TASK: insert the supplied signature object into the DB
-
-    res.status(201).json({
-      status: "success",
-      data: {
-        signature: createdSignature, //return the relevant data (including its db-generated id)
-      },
-    });
-  } else {
-    res.status(400).json({
-      status: "fail",
-      data: {
-        name: "A string value for name is required in your JSON body",
-      },
-    });
-  }
-});
-
-//update a signature.
-app.put("/signatures/:id", async (req, res) => {
-  //  :id refers to a route parameter, which will be made available in req.params.id
-  const { name, message } = req.body;
-  const id = parseInt(req.params.id);
-  if (typeof name === "string") {
-
-    const result: any = null; //FIXME-TASK: update the signature with given id in the DB.
-
-    if (result.rowCount === 1) {
-      const updatedSignature = result.rows[0];
-      res.status(200).json({
-        status: "success",
-        data: {
-          signature: updatedSignature,
-        },
-      });
-    } else {
-      res.status(404).json({
-        status: "fail",
-        data: {
-          id: "Could not find a signature with that id identifier",
-        },
-      });
-
-    }
-  } else {
-    res.status(400).json({
-      status: "fail",
-      data: {
-        name: "A string value for name is required in your JSON body",
-      },
-    });
-  }
-});
-
-app.delete("/signatures/:id", async (req, res) => {
-  const id = parseInt(req.params.id); // params are string type
-
-  const queryResult: any = null; ////FIXME-TASK: delete the row with given id from the db  
-  const didRemove = queryResult.rowCount === 1;
-
-  if (didRemove) {
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods/DELETE#responses
-    // we've gone for '200 response with JSON body' to respond to a DELETE
-    //  but 204 with no response body is another alternative:
-    //  res.status(204).send() to send with status 204 and no JSON body
-    res.status(200).json({
-      status: "success",
-    });
-  } else {
-    res.status(404).json({
-      status: "fail",
-      data: {
-        id: "Could not find a signature with that id identifier",
+        id: "Could not find a result with that id identifier",
       },
     });
   }
